774d91ad187056d28825d3f0095cfedbfb490c93
Hallie Sala <halliesala19@gmail.com>
2023-09-12 15:08:07 -0400

    pair programming

--- server/app.py
+++ server/app.py
@@ -17,16 +17,63 @@ db.init_app(app)
 def home():
     return '<h1>Zoo app</h1>'
 
+# @app.route('/animal')
+# def animal():
+#     return """
+#     <ul> 
+#     </ul>
+#     """
+
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+    if animal:
+        # return f"""
+        # <ul>
+        #     <li name="Name">{animal.name}</li>
+        #     <li name="Species"{animal.species}</li>
+        # </ul>
+        # """
+        return f"""
+            <ul>Name: {animal.name}</ul>
+            <ul>Species: {animal.species}</ul>
+            <ul>Zookeeper: {animal.zookeeper.name}</ul>
+            <ul>Enclosure: {animal.enclosure.environment}</ul>
+        """
+    return "No such animal"
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    if zookeeper:
+        return f"""
+            <ul>Name: {zookeeper.name}</ul>
+            <ul>Birthday: {zookeeper.birthday}</ul>
+            {
+                "".join([
+                    f"<ul>Animal: {animal.name} </ul>"
+                    for animal
+                    in zookeeper.animals
+                ])
+            }
+        """
+    return 'No such zookeeper'
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    if enclosure:
+        return f"""
+            <ul>Environment: {enclosure.environment}</ul>
+            <ul>Open to Visitors: {"True" if enclosure.open_to_visitors else "False"}</ul>
+            {
+                "".join([
+                    f"<ul>Animal: {animal.name} </ul>"
+                    for animal
+                    in enclosure.animals
+                ])
+            }
+        """
     return ''
 
 
